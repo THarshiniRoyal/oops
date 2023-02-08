63e0ab07954820f8843db12aa580dff75f130a85
THarshiniRoyal <115178073+THarshiniRoyal@users.noreply.github.com>
2023-02-08 16:40:33 +0530

    Add files via upload

---
+++ swapping.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"enter the value of a:";
+	cin>>a;
+	cout<<"enter the value of b:";
+	cin>>b;
+	a=a+b;
+	b=a-b;
+	a=a-b;
+	cout<<"a="<<a<<"b="<<b<<endl;
+	return 0;
+}
