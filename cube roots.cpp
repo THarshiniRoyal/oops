c40ba1d40d1bb70ea874157c5ff4f0e41fc935f0
THarshiniRoyal <115178073+THarshiniRoyal@users.noreply.github.com>
2023-02-13 19:41:30 +0530

    Add files via upload

---
+++ cube roots.cpp
@@ -0,0 +1,21 @@
+using namespace std;
+#include<iostream>
+class cube
+{
+public:
+int a, b;
+cube()
+{
+	cout<<" \nEnter the number of finding cube";
+	cin>>a;
+	cout<<" \nThe cube root of number "<<a*a*a;
+	}	
+	
+	
+};
+
+int main()
+{
+	cout<<"Demonstration for  finding cube root of a number";
+	cube a;
+}
